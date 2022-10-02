5be4426bdef029105dddedc13f780e639bfa2728
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-10-03 00:29:31 +0300

    Modify code

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1f1cee058bd04194b793f8942c30b7e-218399767.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
